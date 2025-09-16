6a7b053254f498d49ee587d134a62ad45799792c
profRonaldoIFPI <ronaldo.pb@ifpi.edu.br>
2025-09-16 19:54:21 -0300

    Aula de 16/9: new task + del task

--- Aula 5/ToDoList/app.js
+++ Aula 5/ToDoList/app.js
@@ -1,6 +1,8 @@
 const fs = require("fs") // File System: permite ler e gravar arquivos
 const express = require('express');
 const app = express();
+// import v4 from 'uuid/v4'; //jeito moderno
+const v4 = require('uuid').v4
 
 app.set("view engine", "ejs") //configura o motor de render
 app.use(express.urlencoded({extended: true})) //decondificador POST
@@ -22,6 +24,30 @@ const saveTasks = (tasks)=>{
 app.get("/tasks",(req,res)=>{
     const tasks = loadTask() 
     res.render("index",{tasks});
+})
+
+app.get("/newTask", (req,res)=>{
+    res.render("newTask")
+})
+
+app.post("/tasks",(req,res)=>{
+    const tasks = loadTask() 
+    const newTask = {
+        id: v4(),
+        title: req.body.title,
+        completed: false
+    }
+    tasks.push(newTask)
+    saveTasks(tasks)
+    res.redirect("/tasks")
+})
+
+app.get("/delTasks/:id", (req, res)=>{
+    let tasks = loadTask() 
+    const taskId = req.params.id
+    tasks = tasks.filter(task => task.id != taskId) 
+    saveTasks(tasks)
+    res.redirect("/tasks")
 })
 
 app.get("/tasks/:id", (req, res)=>{
